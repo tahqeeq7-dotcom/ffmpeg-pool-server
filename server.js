2fad28e5345f75aa7f168783d5b3bab1c01d1e42
tahqeeq7-dotcom <tahqeeq7@gmail.com>
2026-07-23 16:36:29 +0500

    switch driveUrl to direct download (uc?export=download) instead of alt=media API

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ function downloadFile(urlStr, destPath, taskId, { fileId, apiKey } = {}) {
 }
 
 function driveUrl(fileId, apiKey) {
-  return 'https://www.googleapis.com/drive/v3/files/' + fileId + '?alt=media&key=' + apiKey;
+  return 'https://drive.google.com/uc?export=download&confirm=1&id=' + fileId;
 }
 
 // Resolve a Google Drive shortcut to its target file ID.
